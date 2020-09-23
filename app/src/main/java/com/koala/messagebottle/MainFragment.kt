a638c974cc978d2bfe03e9fb7a22731134a21861
Marc Tatham <marctatham@gmail.com>
2020-09-23 21:22:49 +0100

    main fragment will be a springboard for the other flows and functionality

--- app/src/main/java/com/koala/messagebottle/MainFragment.kt
+++ app/src/main/java/com/koala/messagebottle/MainFragment.kt
@@ -1,11 +1,16 @@
 package com.koala.messagebottle
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import com.koala.messagebottle.login.LoginActivity
+
+private const val REQUEST_CODE_LOGIN = 100
 
 class MainFragment : Fragment() {
 
@@ -19,7 +24,18 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        val rootView = inflater.inflate(R.layout.main_fragment, container, false)
+
+        val btnSignIn = rootView.findViewById<Button>(R.id.btnSignIn)
+        val btnGetMessage = rootView.findViewById<Button>(R.id.btnGetMessage)
+        val btnPostMessage = rootView.findViewById<Button>(R.id.btnPostMessage)
+
+        btnSignIn.setOnClickListener {
+            val intent = Intent(requireActivity(), LoginActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE_LOGIN)
+        }
+
+        return rootView
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
