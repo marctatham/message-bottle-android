1173a1579dae70d9e7bf059494f2657b49ea16ff
Marc Tatham <marctatham@gmail.com>
2020-09-27 01:06:15 +0100

    simulating retrieval from server
    we take one of a list of hardcoded messages

--- app/src/main/java/com/koala/messagebottle/getmessage/GetMessageViewModel.kt
+++ app/src/main/java/com/koala/messagebottle/getmessage/GetMessageViewModel.kt
@@ -13,12 +13,18 @@ class GetMessageViewModel @Inject constructor() : ViewModel() {
     private val _state = MutableLiveData<MessageState>(MessageState.Loading)
     val state: LiveData<MessageState> = _state
 
+    // TODO: retrieve messages from remote host, for now lets simply simulate it
+    val messages = listOf(
+        "\uD83D\uDC28I love you\uD83C\uDF37\n ♥️prettygirl♥",
+        "♥Holaaa♥\n\uD83D\uDC28Guapaaa\uD83C\uDF37",
+        "\uD83C\uDF35\uD83E\uDDC0\uD83D\uDCF7\uD83D\uDC28\uD83C\uDFD6"
+    )
+
     fun initialise() = viewModelScope.launch {
         // simply simulate fetching from remote server for now
         delay(1000L)
 
-        _state.value =
-            MessageState.MessageReceived("\uD83D\uDC28I love you\uD83C\uDF37\n ♥️prettygirl♥")
+        _state.value = MessageState.MessageReceived(messages.shuffled().first())
     }
 }
 
