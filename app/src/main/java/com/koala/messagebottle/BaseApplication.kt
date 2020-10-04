d82cb4036d7e7c2e5e70407d9fc5b5a8b7a61209
Marc Tatham <marctatham@gmail.com>
2020-10-04 19:38:49 +0100

    some minor cleanup of old commented code

--- app/src/main/java/com/koala/messagebottle/BaseApplication.kt
+++ app/src/main/java/com/koala/messagebottle/BaseApplication.kt
@@ -7,11 +7,6 @@ import com.koala.messagebottle.di.DaggerAppComponent
 
 open class BaseApplication : Application() {
 
-//    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
-//        return DaggerApplicationComponent.factory().create(applicationContext)
-//    }
-
-    // Instance of the AppComponent that will be used by all the Activities in the project
     val appComponent: AppComponent by lazy {
         initializeComponent()
     }
@@ -21,20 +16,4 @@ open class BaseApplication : Application() {
         // We pass the applicationContext that will be used as Context in the graph
         return DaggerAppComponent.factory().create(applicationContext)
     }
-
-
-//    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
-//        return appComponent
-//    }
-
-
-//    val appComponent: ApplicationComponent by lazy {
-//        initializeComponent()
-//    }
-//
-//    open fun initializeComponent(): ApplicationComponent {
-//        // Creates an instance of AppComponent using its Factory constructor
-//        // We pass the applicationContext that will be used as Context in the graph
-//        return DaggerApplicationComponent.factory().create(applicationContext)
-//    }
 }
