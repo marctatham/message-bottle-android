2e55cb028b393f3cc731c48c5177a5979f2740a8
Marc Tatham <marctatham@gmail.com>
2020-10-10 20:30:44 +0100

    added some lottie animations for fun 🤷‍

--- app/src/main/java/com/koala/messagebottle/getmessage/GetMessageFragment.kt
+++ app/src/main/java/com/koala/messagebottle/getmessage/GetMessageFragment.kt
@@ -1,5 +1,6 @@
 package com.koala.messagebottle.getmessage
 
+import android.animation.Animator
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -12,14 +13,16 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.observe
+import com.airbnb.lottie.LottieAnimationView
 import com.google.android.material.snackbar.Snackbar
 import com.koala.messagebottle.R
 import com.koala.messagebottle.home.HomeActivity
-import kotlinx.android.synthetic.main.activity_login.*
 import javax.inject.Inject
 
 class GetMessageFragment : Fragment() {
 
+    private lateinit var containerView: View
+    private lateinit var animView: LottieAnimationView
     private lateinit var progressBar: ProgressBar
     private lateinit var txtMessage: TextView
     private lateinit var btnGetAnotherMessage: Button
@@ -47,9 +50,13 @@ class GetMessageFragment : Fragment() {
     ): View {
         val rootView = inflater.inflate(R.layout.get_message_fragment, container, false)
 
+        containerView = rootView.findViewById(R.id.container)
+        animView = rootView.findViewById(R.id.animBottle)
         txtMessage = rootView.findViewById(R.id.txtMessage)
         progressBar = rootView.findViewById(R.id.progressBar)
         btnGetAnotherMessage = rootView.findViewById(R.id.btnGetMessage)
+
+        animView.enableMergePathsForKitKatAndAbove(true)
         btnGetAnotherMessage.setOnClickListener {
             viewModel.getNewMessage()
         }
@@ -61,7 +68,7 @@ class GetMessageFragment : Fragment() {
                 is MessageState.MessageReceived -> {
                     val messageEntity = state.messageEntity
                     hideLoadingState()
-                    txtMessage.text = messageEntity.message
+                    beginDisplayingMessage(messageEntity.message)
                 }
 
                 MessageState.Failure -> {
@@ -72,6 +79,32 @@ class GetMessageFragment : Fragment() {
         }
 
         return rootView
+    }
+
+    private fun beginDisplayingMessage(message: String) {
+        val animationListener = object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator?) {
+                txtMessage.visibility = View.INVISIBLE
+                txtMessage.animate().alpha(0.0f)
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                txtMessage.visibility = View.VISIBLE
+                txtMessage.animate().alpha(1.0f)
+                animView.removeAllAnimatorListeners()
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+                animView.removeAllAnimatorListeners()
+            }
+
+            override fun onAnimationRepeat(animation: Animator?) {}
+        }
+
+        animView.addAnimatorListener(animationListener)
+        animView.playAnimation()
+        txtMessage.text = message
+
     }
 
     private fun showLoadingState() {
@@ -85,7 +118,7 @@ class GetMessageFragment : Fragment() {
     }
 
     private fun notifyFailure() = Snackbar
-        .make(container, R.string.snack_get_message_failed, Snackbar.LENGTH_SHORT)
+        .make(containerView, R.string.snack_get_message_failed, Snackbar.LENGTH_SHORT)
         .show()
 
     companion object {
