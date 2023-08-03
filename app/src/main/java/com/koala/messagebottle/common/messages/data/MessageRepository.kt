b5a549082855ad684d24d6b10363848187098694
Marc Tatham <marctatham@gmail.com>
2023-08-03 12:04:55 +0200

    messageRepository & DI setup

---
+++ app/src/main/java/com/koala/messagebottle/common/messages/data/MessageRepository.kt
@@ -0,0 +1,42 @@
+package com.koala.messagebottle.common.messages.data
+
+import com.koala.messagebottle.common.messages.domain.MessageEntity
+import com.koala.messagebottle.common.threading.DispatcherIO
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import timber.log.Timber
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class MessageRepository @Inject constructor(
+    private val messageService: MessageService,
+    private val mapper: MessageDataModelMapper,
+    @DispatcherIO private val dispatcherNetwork: CoroutineDispatcher
+) {
+
+    suspend fun getMessage(): MessageEntity {
+        Timber.v("Retrieving message from remote service")
+        val messageDataModel = withContext(dispatcherNetwork) {
+            messageService.getMessage()
+        }
+
+        Timber.v("returning response ${Thread.currentThread().name}")
+        return mapper.mapFrom(messageDataModel)
+    }
+
+    suspend fun getMessages(): List<MessageEntity> {
+        Timber.v("Retrieving ALL messages from remote service")
+        val messageDataModels = withContext(dispatcherNetwork) { messageService.getMessages() }
+        return messageDataModels.map { mapper.mapFrom(it) }
+    }
+
+    suspend fun postMessage(messageEntity: MessageEntity) {
+        Timber.v("Posting message to remote service")
+        withContext(dispatcherNetwork) {
+            val messageDataModel = mapper.mapTo(messageEntity)
+            messageService.postMessage(messageDataModel)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/koala/messagebottle/common/messages/di/MessageModule.kt
@@ -0,0 +1,21 @@
+package com.koala.messagebottle.common.messages.di
+
+import com.koala.messagebottle.common.messages.data.MessageService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class MessageModule {
+
+    @Provides
+    @Singleton
+    fun providesMessageService(retrofit: Retrofit): MessageService {
+        return retrofit.create(MessageService::class.java)
+    }
+
+}
