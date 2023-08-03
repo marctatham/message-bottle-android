6346c739800c96998ab184830cf827cdfb1e0caf
Marc Tatham <marctatham@gmail.com>
2023-08-03 15:47:52 +0200

    comment modules out for now
    - these are going to be a lot different with hilt

--- app/src/main/java/com/koala/messagebottle/login/di/ThirdPartyLoginProviderModule.kt
+++ app/src/main/java/com/koala/messagebottle/login/di/ThirdPartyLoginProviderModule.kt
@@ -1,26 +1,26 @@
-package com.koala.messagebottle.login.di
-
-import androidx.fragment.app.FragmentManager
-import com.koala.messagebottle.di.scope.ActivityScope
-import com.koala.messagebottle.login.LoginActivity
-import com.koala.messagebottle.login.ThirdPartyLoginProvider
-import com.koala.messagebottle.login.google.GoogleLoginProvider
-import dagger.Module
-import dagger.Provides
-
-@Module
-class ThirdPartyLoginProviderModule {
-
-    @ActivityScope
-    @Provides
-    fun provideFragmentManager(loginActivity: LoginActivity): FragmentManager {
-        return loginActivity.supportFragmentManager
-    }
-
-    @ActivityScope
-    @Provides
-    fun providesThirdPartyLoginProvider(fragmentManager: FragmentManager): ThirdPartyLoginProvider {
-        return GoogleLoginProvider(fragmentManager)
-    }
-
-}
+//package com.koala.messagebottle.login.di
+//
+//import androidx.fragment.app.FragmentManager
+//import com.koala.messagebottle.di.scope.ActivityScope
+//import com.koala.messagebottle.login.LoginActivity
+//import com.koala.messagebottle.login.ThirdPartyLoginProvider
+//import com.koala.messagebottle.login.google.GoogleLoginProvider
+//import dagger.Module
+//import dagger.Provides
+//
+//@Module
+//class ThirdPartyLoginProviderModule {
+//
+//    @ActivityScope
+//    @Provides
+//    fun provideFragmentManager(loginActivity: LoginActivity): FragmentManager {
+//        return loginActivity.supportFragmentManager
+//    }
+//
+//    @ActivityScope
+//    @Provides
+//    fun providesThirdPartyLoginProvider(fragmentManager: FragmentManager): ThirdPartyLoginProvider {
+//        return GoogleLoginProvider(fragmentManager)
+//    }
+//
+//}
