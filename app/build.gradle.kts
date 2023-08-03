0651712506520cd129adb4618c2e1afc8c29c120
Marc Tatham <marctatham@gmail.com>
2023-08-03 15:43:59 +0200

    introduced parcelize

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.parcelize)
     alias(libs.plugins.kotlin.kapt)
     alias(libs.plugins.hilt.gradle)
     alias(libs.plugins.ksp)
