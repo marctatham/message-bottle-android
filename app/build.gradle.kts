1b7675f9ae4092abc464c4afe4f7cf0af623c8a4
Marc Tatham <marctatham@gmail.com>
2023-08-04 11:20:35 +0200

    minSdk bumped to 33 for now

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         applicationId = "com.koala.messagebottle"
-        minSdk = 21
+        minSdk = 33
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
