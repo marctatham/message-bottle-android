29d61d6c56cf9285307e62627078d13dea875152
Marc Tatham <marctatham@gmail.com>
2023-08-08 12:37:38 +0200

    ensure we're planting the timber tree!

--- app/src/main/java/com/koala/messagebottle/MessageInABottle.kt
+++ app/src/main/java/com/koala/messagebottle/MessageInABottle.kt
@@ -17,7 +17,42 @@
 package com.koala.messagebottle
 
 import android.app.Application
+import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber.*
+import timber.log.Timber.Forest.plant
+
 
 @HiltAndroidApp
-class MessageInABottle : Application()
+class MessageInABottle : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        val isDebug = true //BuildConfig.DEBUG
+        if (isDebug) {
+            plant(DebugTree())
+        } else {
+            plant(CrashReportingTree())
+        }
+    }
+
+    /** A tree which logs important information for crash reporting.  */
+    private class CrashReportingTree : Tree() {
+        override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+            if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+                return
+            }
+
+            //TODO: plug in crashlytics
+//            FakeCrashLibrary.log(priority, tag, message)
+//            if (t != null) {
+//                if (priority == Log.ERROR) {
+//                    FakeCrashLibrary.logError(t)
+//                } else if (priority == Log.WARN) {
+//                    FakeCrashLibrary.logWarning(t)
+//                }
+//            }
+        }
+    }
+}
+
