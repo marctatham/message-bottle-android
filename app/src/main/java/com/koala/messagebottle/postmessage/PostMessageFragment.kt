97fd41db8b445df1f0a26751fcab4905ab106b7e
Marc Tatham <marctatham@gmail.com>
2023-08-08 13:53:40 +0200

    re-introduced PostMessage functionality

--- app/src/main/java/com/koala/messagebottle/postmessage/PostMessageFragment.kt
+++ app/src/main/java/com/koala/messagebottle/postmessage/PostMessageFragment.kt
@@ -13,77 +13,74 @@ import com.google.android.material.snackbar.Snackbar
 import com.koala.messagebottle.R
 import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class PostMessageFragment : Fragment() {
 
-}
+    private lateinit var containerView: View
+    private lateinit var progressBar: ProgressBar
+    private lateinit var edtMessage: EditText
+    private lateinit var btnPostMessage: Button
 
-//@AndroidEntryPoint
-//class PostMessageFragment : Fragment() {
-//
-//    private lateinit var containerView: View
-//    private lateinit var progressBar: ProgressBar
-//    private lateinit var edtMessage: EditText
-//    private lateinit var btnPostMessage: Button
-//
-//    private val viewModel: PostMessageViewModel by hiltNavGraphViewModels(R.id.nav_host_fragment)
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View {
-//        val rootView = inflater.inflate(R.layout.post_message_fragment, container, false)
-//
-//        containerView = rootView.findViewById(R.id.container)
-//        edtMessage = rootView.findViewById(R.id.edtMessage)
-//        progressBar = rootView.findViewById(R.id.progressBar)
-//        btnPostMessage = rootView.findViewById(R.id.btnPostMessage)
-//        btnPostMessage.setOnClickListener {
-//            val message = edtMessage.text.toString()
-//            viewModel.postMessage(message)
-//        }
-//
-//        viewModel.state.observe(viewLifecycleOwner) { state: MessageState ->
-//            when (state) {
-//                MessageState.Idle -> hideProgressBar()
-//
-//                MessageState.Loading -> showProgressBar()
-//
-//                is MessageState.MessagePosted -> {
-//                    hideProgressBar()
-//
-//                    val notification = getString(
-//                        R.string.snack_posted_message,
-//                        state.messageEntity.message
-//                    )
-//
-//                    edtMessage.text.clear()
-//
-//                    Snackbar.make(containerView, notification, Snackbar.LENGTH_SHORT).show()
-//                }
-//
-//                MessageState.Failure -> {
-//                    hideProgressBar()
-//                    displayPostMessageFailed()
-//                }
-//            }
-//        }
-//
-//        return rootView
-//    }
-//
-//    private fun showProgressBar() {
-//        progressBar.visibility = View.VISIBLE
-//    }
-//
-//    private fun hideProgressBar() {
-//        progressBar.visibility = View.INVISIBLE
-//    }
-//
-//    private fun displayPostMessageFailed() = Snackbar
-//        .make(containerView, R.string.snack_post_message_failed, Snackbar.LENGTH_SHORT)
-//        .show()
-//
-//    companion object {
-//        fun newInstance() = PostMessageFragment()
-//    }
-//}
+    private val viewModel: PostMessageViewModel by hiltNavGraphViewModels(R.id.app_nav)
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val rootView = inflater.inflate(R.layout.post_message_fragment, container, false)
+
+        containerView = rootView.findViewById(R.id.container)
+        edtMessage = rootView.findViewById(R.id.edtMessage)
+        progressBar = rootView.findViewById(R.id.progressBar)
+        btnPostMessage = rootView.findViewById(R.id.btnPostMessage)
+        btnPostMessage.setOnClickListener {
+            val message = edtMessage.text.toString()
+            viewModel.postMessage(message)
+        }
+
+        viewModel.state.observe(viewLifecycleOwner) { state: MessageState ->
+            when (state) {
+                MessageState.Idle -> hideProgressBar()
+
+                MessageState.Loading -> showProgressBar()
+
+                is MessageState.MessagePosted -> {
+                    hideProgressBar()
+
+                    val notification = getString(
+                        R.string.snack_posted_message,
+                        state.messageEntity.message
+                    )
+
+                    edtMessage.text.clear()
+
+                    Snackbar.make(containerView, notification, Snackbar.LENGTH_SHORT).show()
+                }
+
+                MessageState.Failure -> {
+                    hideProgressBar()
+                    displayPostMessageFailed()
+                }
+            }
+        }
+
+        return rootView
+    }
+
+    private fun showProgressBar() {
+        progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressBar() {
+        progressBar.visibility = View.INVISIBLE
+    }
+
+    private fun displayPostMessageFailed() = Snackbar
+        .make(containerView, R.string.snack_post_message_failed, Snackbar.LENGTH_SHORT)
+        .show()
+
+    companion object {
+        fun newInstance() = PostMessageFragment()
+    }
+}
