3ba7c8afa7c2088757c5bc447332ff0320f25487
Marc Tatham <marctatham@gmail.com>
2023-08-09 14:43:06 +0200

    class cleanup

--- app/src/main/java/com/koala/messagebottle/common/authentication/domain/UserEntity.kt
+++ app/src/main/java/com/koala/messagebottle/common/authentication/domain/UserEntity.kt
@@ -1,7 +1,5 @@
 package com.koala.messagebottle.common.authentication.domain
 
-// for now a logged in user doesn't actually provide any additional functionality but since
-// this is destined to change in the near future, it's being integrated as a 1st class citizen
 sealed class UserEntity {
 
     data class AuthenticatedUser(
@@ -9,7 +7,5 @@ sealed class UserEntity {
         val jwtToken: String
     ) : UserEntity()
 
-    // as opposed to anonymously authenticated user
     object UnauthenticatedUser : UserEntity()
-
 }
