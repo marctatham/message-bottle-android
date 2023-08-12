d2a7e9f032c186b9636cc8c28e511fa55741aa87
Marc Tatham <marctatham@gmail.com>
2023-08-12 14:43:09 +0200

    removed JVM static annotations

--- app/src/main/java/com/koala/messagebottle/common/threading/di/DispatchersModule.kt
+++ app/src/main/java/com/koala/messagebottle/common/threading/di/DispatchersModule.kt
@@ -16,21 +16,17 @@ object DispatchersModule {
 
     @Provides
     @DispatcherIO
-    @JvmStatic
     fun providesDispatcherIO() = Dispatchers.IO
 
     @Provides
     @DispatcherMain
-    @JvmStatic
     fun providesDispatcherMain() = Dispatchers.Main
 
     @Provides
     @DispatcherDefault
-    @JvmStatic
     fun providesDispatcherDefault() = Dispatchers.Default
 
     @Provides
     @DispatcherUnconfined
-    @JvmStatic
     fun providesDispatcherUnConfined() = Dispatchers.Unconfined
 }
