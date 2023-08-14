09461bbe79e153334f1e2811aeb89e45b1ce2095
Marc Tatham <marctatham@gmail.com>
2023-08-14 19:20:51 +0200

    simple relocation to domain subpackage, no logical changes

--- app/src/main/java/com/koala/messagebottle/main/postmessage/domain/PostMessageUseCase.kt
+++ app/src/main/java/com/koala/messagebottle/main/postmessage/domain/PostMessageUseCase.kt
@@ -1,4 +1,4 @@
-package com.koala.messagebottle.main.postmessage.ui
+package com.koala.messagebottle.main.postmessage.domain
 
 import com.koala.messagebottle.common.authentication.domain.IAuthenticationRepository
 import com.koala.messagebottle.common.authentication.domain.UserEntity
