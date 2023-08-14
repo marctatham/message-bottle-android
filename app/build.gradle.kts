5459ffcf638b4b0da8b0e868e6fb07d42d77e47d
Marc Tatham <marctatham@gmail.com>
2023-08-14 19:01:27 +0200

    added meaningful unit tests of PostMessageUseCase
    - added mockK & kotlin-test dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -150,9 +150,10 @@ dependencies {
     // Local tests: jUnit, coroutines, Android runner
     testImplementation(libs.junit)
     testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.mockk)
+    testImplementation(libs.kotlinx.test)
 
     // Instrumented tests: jUnit rules and runners
-
     androidTestImplementation(libs.androidx.test.core)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.androidx.test.runner)
---
+++ app/src/test/java/com/koala/messagebottle/main/postmessage/PostMessageUseCaseTest.kt
@@ -0,0 +1,57 @@
+package com.koala.messagebottle.main.postmessage
+
+import com.koala.messagebottle.common.authentication.domain.IAuthenticationRepository
+import com.koala.messagebottle.common.authentication.domain.ProviderType
+import com.koala.messagebottle.common.authentication.domain.UserEntity
+import com.koala.messagebottle.common.messages.domain.IMessageRepository
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * Unit tests for [PostMessageUseCase].
+ */
+@OptIn(ExperimentalCoroutinesApi::class) // TODO: Remove when stable
+class PostMessageUseCaseTest {
+
+    @Test
+    fun `when user has not authenticated, they should not be able to post`() = runTest {
+        // given
+        val messageRepo = mockk<IMessageRepository>()
+        val authRepo = mockk<IAuthenticationRepository>()
+        val useCase = PostMessageUseCase(authRepo, messageRepo)
+        every { authRepo.user.value } returns UserEntity.UnauthenticatedUser
+
+        // when
+        val result: PostMessageResult = useCase.postMessage("This should not be allowed")
+
+        // then
+        Assert.assertEquals(PostMessageResult.Unauthenticated, result)
+    }
+
+
+    @Test
+    fun `when user has authenticated, they are able to post anything they want`() = runTest {
+        // given
+        val messageRepo = mockk<IMessageRepository>()
+        val authRepo = mockk<IAuthenticationRepository>()
+        val useCase = PostMessageUseCase(authRepo, messageRepo)
+        val authenticatedUser =
+            UserEntity.AuthenticatedUser(ProviderType.Google, "myFakeToken", "myFakeUserId")
+        every { authRepo.user.value } returns authenticatedUser
+        coEvery { messageRepo.postMessage(any()) } returns Unit
+
+        // when
+        val result: PostMessageResult =
+            useCase.postMessage("I should be allowed to post my message")
+
+        // then
+        coVerify(exactly = 1) { messageRepo.postMessage(any()) }
+        Assert.assertTrue(result is PostMessageResult.Success)
+    }
+}
