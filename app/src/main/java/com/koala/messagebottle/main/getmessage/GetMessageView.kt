f3fd8506df08ae654e2717bc321f1e60a71f8e60
Marc Tatham <marctatham@gmail.com>
2023-09-04 13:32:08 +0200

    introduced fade transition of text component

--- app/src/main/java/com/koala/messagebottle/main/getmessage/GetMessageView.kt
+++ app/src/main/java/com/koala/messagebottle/main/getmessage/GetMessageView.kt
@@ -1,5 +1,8 @@
 package com.koala.messagebottle.main.getmessage
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,6 +17,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,9 +26,11 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.koala.messagebottle.R
 import com.koala.messagebottle.common.compose.fakeClickHandler
+import timber.log.Timber
 
 @Composable
 fun GetMessageView(
@@ -35,18 +42,25 @@ fun GetMessageView(
 
         MessageState.Loading -> FullScreenLoadingView()
 
-        is MessageState.MessageReceived -> GetMessageSuccessView(onNewMessageHandler = viewModel::getNewMessage)
+        is MessageState.MessageReceived -> GetMessageSuccessView(
+            message = (uiState as MessageState.MessageReceived).messageEntity.message,
+            onNewMessageHandler = viewModel::getNewMessage
+        )
     }
 }
 
 @Preview
 @Composable
 fun FullScreenLoadingView() {
-    CircularProgressIndicator(
-        modifier = Modifier.fillMaxSize(0.4f),
-        color = MaterialTheme.colorScheme.surfaceVariant,
-        trackColor = MaterialTheme.colorScheme.secondary,
-    )
+    Box(modifier = Modifier.fillMaxSize()) {
+        CircularProgressIndicator(
+            modifier = Modifier
+                .fillMaxSize(0.35f)
+                .align(Alignment.Center),
+            color = MaterialTheme.colorScheme.surfaceVariant,
+            trackColor = MaterialTheme.colorScheme.secondary,
+        )
+    }
 }
 
 @Preview
@@ -79,25 +93,56 @@ fun GetMessageFailedView(
 @Preview
 @Composable
 fun GetMessageSuccessView(
+    message: String = "Hello world!",
     onNewMessageHandler: () -> Unit = fakeClickHandler
 ) {
     val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.message_from_bottle))
+    val progress: Float by animateLottieCompositionAsState(
+        composition = composition,
+        iterations = 1
+    )
+
+    val isTextVisible: Boolean = progress == 1f
+
+    // Animate the alpha property from 0 to 1 when isVisible is true
+    val alpha: Float by animateFloatAsState(
+        //targetValue = 1f, // else 0f,
+        targetValue = if (isTextVisible) 1f else 0f,
+        animationSpec = tween(
+            durationMillis = 2000, // Animation duration
+            easing = FastOutSlowInEasing // Easing function
+        ), label = "Text Fade In"
+    )
+
+    Timber.d("AlphaDebug Alpha: $alpha")
 
     Box(modifier = Modifier.fillMaxSize()) {
         LottieAnimation(
             composition = composition,
+            progress = { progress },
             modifier = Modifier
                 .align(Alignment.TopCenter)
                 .fillMaxSize(),
         )
 
-        // TODO: introduce text view to display message
+        // Text view that becomes visible after Lottie animation completion
+        if (progress == 1f) {
+            Text(
+                text = message,
+                color = Color.White.copy(alpha = alpha),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .alpha(alpha)
+            )
+        }
 
         Button(
             onClick = onNewMessageHandler,
             modifier = Modifier
                 .align(Alignment.BottomCenter)
                 .fillMaxWidth()
+                .padding(16.dp)
         ) { Text(text = "Get another message from a bottle") }
     }
 }
