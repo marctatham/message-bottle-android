63a9900b0f1625686c4d3d5a6c779672c0851c8b
Marc Tatham <marctatham@gmail.com>
2023-09-08 15:28:57 +0200

    baseline migration of POST to compose

--- app/src/main/java/com/koala/messagebottle/main/postmessage/ui/PostMessageFragment.kt
+++ app/src/main/java/com/koala/messagebottle/main/postmessage/ui/PostMessageFragment.kt
@@ -4,100 +4,34 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ProgressBar
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
-import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import com.google.android.material.snackbar.Snackbar
-import com.koala.messagebottle.R
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 
 @AndroidEntryPoint
 class PostMessageFragment : Fragment() {
 
-    private lateinit var containerView: View
-    private lateinit var progressBar: ProgressBar
-    private lateinit var edtMessage: EditText
-    private lateinit var btnPostMessage: Button
-
-    private val viewModel: PostMessageViewModel by hiltNavGraphViewModels(R.id.app_nav)
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.state.collect { state: MessageState ->
-                    when (state) {
-                        MessageState.Idle -> hideProgressBar()
-
-                        MessageState.Loading -> showProgressBar()
-
-                        is MessageState.MessagePosted -> {
-                            hideProgressBar()
-
-                            val notification = getString(
-                                R.string.snack_posted_message,
-                                state.messageEntity.message
-                            )
-
-                            edtMessage.text.clear()
-
-                            Snackbar.make(containerView, notification, Snackbar.LENGTH_SHORT).show()
-                        }
-
-                        MessageState.Failure -> {
-                            hideProgressBar()
-                            displayPostMessageFailed()
-                        }
-
-                        MessageState.NotAuthenticated -> {
-                            hideProgressBar()
-                            displayLoginToPost()
-                        }
-                    }
-                }
-            }
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val rootView = inflater.inflate(R.layout.post_message_fragment, container, false)
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
 
-        containerView = rootView.findViewById(R.id.container)
-        edtMessage = rootView.findViewById(R.id.edtMessage)
-        progressBar = rootView.findViewById(R.id.progressBar)
-        btnPostMessage = rootView.findViewById(R.id.btnPostMessage)
-        btnPostMessage.setOnClickListener {
-            val message = edtMessage.text.toString()
-            viewModel.postMessage(message)
+            setContent {
+                PostViewScreen()
+            }
         }
-
-        return rootView
     }
 
-    private fun showProgressBar() {
-        progressBar.visibility = View.VISIBLE
-    }
-
-    private fun hideProgressBar() {
-        progressBar.visibility = View.INVISIBLE
-    }
-
-    private fun displayPostMessageFailed() = Snackbar
-        .make(containerView, R.string.snack_post_message_failed, Snackbar.LENGTH_SHORT)
-        .show()
-
-    private fun displayLoginToPost() = Snackbar
-        .make(containerView, R.string.snack_post_message_requires_auth, Snackbar.LENGTH_SHORT)
-        .show()
+    // TODO: this functionality should be handled by the new compose view
+//    private fun displayPostMessageFailed() = Snackbar
+//        .make(containerView, R.string.snack_post_message_failed, Snackbar.LENGTH_SHORT)
+//        .show()
+//
+//    private fun displayLoginToPost() = Snackbar
+//        .make(containerView, R.string.snack_post_message_requires_auth, Snackbar.LENGTH_SHORT)
+//        .show()
 }
---
+++ app/src/main/java/com/koala/messagebottle/main/postmessage/ui/PostView.kt
@@ -0,0 +1,82 @@
+package com.koala.messagebottle.main.postmessage.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+
+@Composable
+fun PostViewScreen(
+    viewModel: PostMessageViewModel = hiltViewModel(),
+) {
+    val state: MessageState by viewModel.state.collectAsStateWithLifecycle()
+    val onPostHandler: (messageToPost: String) -> Unit = { viewModel.postMessage(it) }
+    PostView(state, onPostHandler)
+}
+
+@Composable
+fun PostView(
+    uiState: MessageState,
+    onPostHandler: (messageToPost: String) -> Unit,
+) {
+    val textState: MutableState<TextFieldValue> = remember { mutableStateOf(TextFieldValue()) }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Spacer(modifier = Modifier.weight(1f))
+
+        if (uiState is MessageState.Loading) {
+            CircularProgressIndicator(
+                modifier = Modifier.size(48.dp),
+                strokeWidth = 4.dp
+            )
+        } else {
+            TextField(
+                value = textState.value,
+                onValueChange = { textState.value = it },
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Button(
+            onClick = { onPostHandler(textState.value.text) },
+            modifier = Modifier.padding(16.dp)
+        ) { Text(text = "Put a message in a bottle") }
+
+    }
+}
+
+@Preview
+@Composable
+fun PostViewPreviewLoading() {
+    PostView(MessageState.Loading, onPostHandler = {})
+}
+
+
+@Preview
+@Composable
+fun PostViewPreviewIdle() {
+    PostView(MessageState.Idle, onPostHandler = {})
+}
