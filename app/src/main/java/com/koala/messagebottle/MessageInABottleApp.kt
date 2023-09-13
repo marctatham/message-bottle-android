f7b15076fde864e32a42ec0b860a7019fcc3a34d
Marc Tatham <marctatham@gmail.com>
2023-09-13 12:54:20 +0200

    first pass of migration to Navigation Compose

---
+++ app/src/main/java/com/koala/messagebottle/MessageInABottleApp.kt
@@ -0,0 +1,39 @@
+package com.koala.messagebottle
+
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.koala.messagebottle.login.LoginViewScreen
+import com.koala.messagebottle.main.getmessage.GetMessageView
+import com.koala.messagebottle.main.home.HomeView
+import com.koala.messagebottle.main.postmessage.ui.PostViewScreen
+import com.koala.messagebottle.main.viewmessages.ViewMessagesScreen
+
+@Composable
+fun MessageInABottleApp() {
+    val navController = rememberNavController()
+    MessageInABottleAppNavHost(navController = navController)
+}
+
+@Composable
+fun MessageInABottleAppNavHost(
+    navController: NavHostController
+) {
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            HomeView(
+                onSignInHandler = { navController.navigate("login") },
+                onGetMessageHandler = { navController.navigate("getMessage") },
+                onPostMessageHandler = { navController.navigate("postMessage") },
+                onViewMessageHandler = { navController.navigate("viewMessages") },
+            )
+        }
+        composable("getMessage") { GetMessageView() }
+        composable("postMessage") { PostViewScreen() }
+        composable("viewMessages") { ViewMessagesScreen() }
+        composable("login") { LoginViewScreen() }
+    }
+}
--- app/src/main/java/com/koala/messagebottle/main/MainActivity.kt
+++ app/src/main/java/com/koala/messagebottle/main/MainActivity.kt
@@ -1,80 +1,21 @@
 package com.koala.messagebottle.main
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
-import androidx.navigation.findNavController
-import com.koala.messagebottle.R
-import com.koala.messagebottle.main.Constants.REQUEST_CODE_LOGIN
+import androidx.core.view.WindowCompat
+import com.koala.messagebottle.MessageInABottleApp
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home_activity)
 
-        // TODO: introduce Theme at top most level
-        // for this, this activity we want to be the single entry point into the application (at least in context of THIS app)
-
-        //setContent {
-        // TODO: use our BottlingTheme of course... this is to jog the ol' memory
-        //            ReplyTheme {
-        //                val windowSize = calculateWindowSizeClass(this)
-        //                val displayFeatures = calculateDisplayFeatures(this)
-        //                val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-        //
-        //                ReplyApp(
-        //                    windowSize = windowSize,
-        //                    displayFeatures = displayFeatures,
-        //                    replyHomeUIState = uiState,
-        //                    closeDetailScreen = {
-        //                        viewModel.closeDetailScreen()
-        //                    },
-        //                    navigateToDetail = { emailId, pane ->
-        //                        viewModel.setOpenedEmail(emailId, pane)
-        //                    },
-        //                    toggleSelectedEmail = { emailId ->
-        //                        viewModel.toggleSelectedEmail(emailId)
-        //                    }
-        //                )
-        //            }
-        //        }
-
-    }
-
-    fun showGetMessage() = findNavController(R.id.nav_host_fragment)
-        .navigate(R.id.action_homeToGetMessage)
-
-    fun showPostMessage() = findNavController(R.id.nav_host_fragment)
-        .navigate(R.id.action_homeToPostMessage)
-
-    fun showViewMessages() = findNavController(R.id.nav_host_fragment)
-        .navigate(R.id.action_homeToVewMessages)
-
-    @Deprecated("Deprecated in Java")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        when (requestCode) {
-
-            REQUEST_CODE_LOGIN -> {
-                handleLogin(resultCode)
-            }
-
-            else -> Timber.w("Unhandled request code")
-        }
-    }
-
-    private fun handleLogin(resultCode: Int) {
-        if (Activity.RESULT_OK == resultCode) {
-            Timber.i("Login is successful")
-        } else {
-            Timber.w("Login has failed, killing app")
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContent {
+            MessageInABottleApp()
         }
     }
 }
