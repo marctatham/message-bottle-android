5ad706e2ea5c95fefd71c5806ea4ad9e67d9b02b
Marc Tatham <marctatham@gmail.com>
2023-09-14 16:19:55 +0200

    HomeScreen design revisited

--- app/src/main/java/com/koala/messagebottle/main/home/HomeScreen.kt
+++ app/src/main/java/com/koala/messagebottle/main/home/HomeScreen.kt
@@ -4,50 +4,85 @@ import CustomBox
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.koala.messagebottle.R
-import com.koala.messagebottle.common.ui.fakeClickHandler
+import com.koala.messagebottle.common.components.BottlingButton
+import com.koala.messagebottle.common.components.BottlingButtonType
 
 @Composable
 fun HomeView(
-    onSignInHandler: () -> Unit,
     onGetMessageHandler: () -> Unit,
     onPostMessageHandler: () -> Unit,
-    onViewMessageHandler: () -> Unit
 ) {
     val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.message_in_bottle))
 
     CustomBox(
         canvasContent = {
-            LottieAnimation(
-                composition = composition,
-                iterations = LottieConstants.IterateForever,
-                speed = 0.6f,
-                modifier = Modifier.fillMaxSize()
-            )
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+
+                LottieAnimation(
+                    composition = composition,
+                    iterations = LottieConstants.IterateForever,
+                    speed = 0.6f,
+                    modifier = Modifier.fillMaxSize(0.6f)
+                )
+
+                Text(
+                    modifier = Modifier.padding(8.dp),
+                    text = stringResource(id = R.string.app_name),
+                    style = MaterialTheme.typography.titleLarge,
+                    textAlign = TextAlign.Center,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+
+                Text(
+                    modifier = Modifier.padding(horizontal = 8.dp),
+                    text = stringResource(id = R.string.home_description),
+                    style = MaterialTheme.typography.bodyMedium,
+                    textAlign = TextAlign.Center,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
+
         },
         outsideCanvasContent = {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Bottom,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxSize()
             ) {
-                Button(onClick = onSignInHandler) { Text(text = "Sign In") }
 
-                Button(onClick = onGetMessageHandler) { Text(text = "Get a message from a bottle") }
+                BottlingButton(
+                    text = R.string.home_button_get_message,
+                    buttonType = BottlingButtonType.PRIMARY,
+                    onTapHandler = onGetMessageHandler,
+                )
 
-                Button(onClick = onPostMessageHandler) { Text(text = "Put a message in a bottle") }
-
-                Button(onClick = onViewMessageHandler) { Text(text = "View all messages") }
+                BottlingButton(
+                    text = R.string.home_button_put_message,
+                    buttonType = BottlingButtonType.SECONDARY,
+                    onTapHandler = onPostMessageHandler,
+                )
             }
         }
     )
@@ -56,10 +91,5 @@ fun HomeView(
 @Preview
 @Composable
 fun HomeViewPreview() {
-    HomeView(
-        fakeClickHandler,
-        fakeClickHandler,
-        fakeClickHandler,
-        fakeClickHandler
-    )
+    HomeView({}, {})
 }
