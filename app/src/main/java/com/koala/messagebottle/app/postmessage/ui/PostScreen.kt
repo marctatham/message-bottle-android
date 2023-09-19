6f04da688b1de12c0fb54dc40160a9c20aa3740c
Marc Tatham <marctatham@gmail.com>
2023-09-19 11:19:30 +0200

    BottlingMessageCard broken out into it's own file
    helps keep the file simpler, and it is co-located
    to where it is used

--- app/src/main/java/com/koala/messagebottle/app/postmessage/ui/PostScreen.kt
+++ app/src/main/java/com/koala/messagebottle/app/postmessage/ui/PostScreen.kt
@@ -1,6 +1,5 @@
 package com.koala.messagebottle.app.postmessage.ui
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,12 +7,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -23,7 +18,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -125,57 +119,6 @@ private fun PostScreen(
         }
 
     }
-}
-
-@Composable
-private fun BottlingMessageCard(
-    value: String,
-    onValueChange: (String) -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    Card(
-        modifier = modifier,
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.background
-        ),
-        border = BorderStroke(1.dp, Color.Companion.Black),
-        shape = MaterialTheme.shapes.large,
-        elevation = CardDefaults.cardElevation(defaultElevation = 20.dp),
-    ) {
-        Text(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 16.dp, top = 16.dp, end = 16.dp),
-            text = stringResource(id = R.string.title_post_message),
-            style = MaterialTheme.typography.titleLarge,
-            textAlign = TextAlign.Center,
-            color = MaterialTheme.colorScheme.onBackground
-        )
-        OutlinedTextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 16.dp, top = 8.dp, end = 16.dp, bottom = 16.dp),
-            onValueChange = onValueChange,
-            value = value,
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedContainerColor = MaterialTheme.colorScheme.background,
-                unfocusedContainerColor = MaterialTheme.colorScheme.background,
-                focusedTextColor = MaterialTheme.colorScheme.onBackground,
-                unfocusedTextColor = MaterialTheme.colorScheme.onBackground,
-            ),
-            maxLines = 15,
-            placeholder = { Text(text = stringResource(R.string.post_message_hint)) },
-        )
-    }
-}
-
-@Preview
-@Composable
-private fun BottlingMessageCard() {
-    BottlingMessageCard(
-        value = "This is my message that I'm going to be bottling",
-        onValueChange = {},
-    )
 }
 
 @Preview
