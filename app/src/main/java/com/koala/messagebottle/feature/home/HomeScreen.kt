71f650480c4ed055524388e0bb2c96b509e5466e
Marc Tatham <marctatham@gmail.com>
2023-09-28 17:15:13 +0200

    tweaked styling ever so slightly

--- app/src/main/java/com/koala/messagebottle/feature/home/HomeScreen.kt
+++ app/src/main/java/com/koala/messagebottle/feature/home/HomeScreen.kt
@@ -96,8 +96,8 @@ private fun HomeScreen(
                         modifier = Modifier.padding(
                             start = 16.dp,
                             end = 16.dp,
-                            top = 8.dp,
-                            bottom = 16.dp
+                            top = 16.dp,
+                            bottom = 8.dp
                         )
                     )
                 } else {
