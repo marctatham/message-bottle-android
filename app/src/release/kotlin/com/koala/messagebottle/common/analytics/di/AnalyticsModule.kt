7aa9a6153fe944329280476127c07d2f12dd8eda
Marc Tatham <marctatham@gmail.com>
2023-10-07 22:32:54 +0200

    fixing a legacy naming issue

--- app/src/release/kotlin/com/koala/messagebottle/common/analytics/di/AnalyticsModule.kt
+++ app/src/release/kotlin/com/koala/messagebottle/common/analytics/di/AnalyticsModule.kt
@@ -1,10 +1,10 @@
-package com.koala.messagebottle.common.analytics.di
+package com.bottlecast.app.common.analytics.di
 
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.ktx.Firebase
-import com.koala.messagebottle.common.analytics.FirebaseTracker
-import com.koala.messagebottle.common.analytics.ITracker
+import com.bottlecast.app.analytics.FirebaseTracker
+import com.bottlecast.app.analytics.ITracker
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
