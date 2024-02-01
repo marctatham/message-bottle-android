d305179b75c38d2d5570cde4dfe6781080b7093f
Marc Tatham <marctatham@gmail.com>
2024-02-01 13:49:27 +0100

    shuffled around to where it belongs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -112,7 +112,6 @@ dependencies {
 
     // Core Android dependencies
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
 
     // Hilt Dependency Injection
@@ -126,6 +125,7 @@ dependencies {
     kaptTest(libs.hilt.android.compiler)
 
     // Arch Components
+    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation.compose)
